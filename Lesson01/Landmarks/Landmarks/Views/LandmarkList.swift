41474f93d5a1f1ee69f50b27fa2b6992b53f9ef6
benjamin liu <bliu7@foxmail.com>
2022-12-24 21:19:33 +0800

    make the list dynamic

---
+++ Lesson01/Landmarks/Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,22 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Benjamin Liu on 2022/12/24.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List(landmarks) { landmark in
+            LandmarkRow(landmark: landmark)
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
