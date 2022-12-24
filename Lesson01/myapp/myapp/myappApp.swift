7d9982e38d199d61f22909de19cf4722e4fb8913
benjamin liu <bliu7@foxmail.com>
2022-12-24 12:14:02 +0800

    swift ui basic

---
+++ Lesson01/myapp/myapp/myappApp.swift
@@ -0,0 +1,17 @@
+//
+//  myappApp.swift
+//  myapp
+//
+//  Created by Benjamin Liu on 2022/12/24.
+//
+
+import SwiftUI
+
+@main
+struct myappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
